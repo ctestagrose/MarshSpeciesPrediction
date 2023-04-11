d37df29d222bf08a1cfda7c70fb75db58c03a67d
Conrad Testagrose <73864022+ctestagrose@users.noreply.github.com>
2023-04-11 18:31:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-import os.path
+# This is the code used to train and test the models used in "Comparative Study Between Vision Transformer and
+# EfficientNet on Marsh Grass Classification". This paper was accepted to FLAIRS 2023 for the main conference track.
 
 import Train, Evaluate
 
-num_epochs = 30
-batch_size = 16
-image_size = 33
+if __name__ == "__main__":
+    # current parameters are inline with paper methodology
+    num_epochs = 50
+    batch_size = 128
+    image_size = 33
 
-model_types = ['ViT', 'efficientnetb0', 'efficientnetb7']
+    model_types = ['ViT', 'efficientnetb0', 'efficientnetb7']
+    data_sets = ['data_set_CV1', 'data_set_CV2', 'data_set_CV3', 'data_set_CV4', 'data_set_CV5']
 
-if os.path.exists("./models") == False:
-    os.makedirs("./models")
+    for mod_type in model_types:
+        for index, dataset in enumerate(data_sets):
+            data_file = './Data_Dictionaries/'+dataset+".json"
+            Train.train(model_type=mod_type, data_file=data_file,
+                        save_file="./MarshSpeciesPrediction/models/"+mod_type + "_CV" + str(index + 1),
+                        batch_size=batch_size, image_size=image_size, num_epochs=num_epochs)
 
-for index, mod_type in enumerate(model_types):
-    Train.train(model_type=mod_type, data_file="./Data_Dictionaries/data_set_CV" + str(index + 1) + ".json",
-          save_file="./models/"+mod_type + "_CV" + str(index + 1),
-          batch_size=batch_size, image_size=image_size, num_epochs=num_epochs)
+            Evaluate.predict(model_type=mod_type, data_file=data_file,
+                             fold="CV" + str(index + 1), image_size=image_size, batch_size=batch_size)
